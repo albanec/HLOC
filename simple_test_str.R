23502301c1f1fb3ce1698e6a38ca4d25866c6df6
albanec <eugeni.maksimov@yandex.ru>
2016-06-16 22:18:01 +0300

    stoone day

--- simple_test_str.R
+++ simple_test_str.R
@@ -7,10 +7,13 @@ to.date <- Sys.Date()
 tickers <- c("SPFB.Si", "SPFB.RTS", "SPFB.BR")
 # загрузка и нормализация данных
 cat("Start Loading Data... ", "\n")
-data.list <- GetData_Ticker_SetList(tickers, from.date, to.date, period)
-
+data.source.list <- GetData_Ticker_SetList(tickers, from.date, to.date, period)
 cat("Start Merging Data... ", "\n")
-data.list <- MergeData_fromAll_toOne(data.list)
+data.source.list <- MergeData_fromAll_toOne(data.source.list)
 cat("Start Normalization&Improve Data... ", "\n")
-data.list[[1]] <- NormData_NA(data=data.list[[1]], type="full")	
-data.list[[1]] <- AddData_FuturesSpecs_forXTS(data=data.list[[1]], from.date, to.date)
+# удаление NA (по свечам)
+data.source.list[[1]] <- NormData_NA(data=data.source.list[[1]], type="full")	
+# добавляем ГО и данные по USDRUB
+data.source.list[[1]] <- AddData_FuturesSpecs_forXTS(data=data.source.list[[1]], from.date, to.date)
+##
+# работа стратегии
