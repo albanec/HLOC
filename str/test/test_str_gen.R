fe9052340aaf32c901cd0b1628cee182160c07cc
albanec <eugeni.maksimov@yandex.ru>
2016-09-19 18:03:25 +0300

    Добавлены функции параллельной оптимизации

--- str/test/test_str_gen.R
+++ str/test/test_str_gen.R
@@ -3,4 +3,6 @@ source("gen/libGen_Mining.R")
 source("gen/libGen_GetData.R")
 source("gen/libGen_Data.R")
 source("gen/libGen_Quotes.R")
+source("gen/libGen_Perfomance.R")
 source("gen/libGeneric.R")
+
--- str/test/test_str_opt.R
+++ str/test/test_str_opt.R
@@ -1,6 +1,70 @@
 #source("str/libStrategy.R")
 #
 ###
+#' Тупая функция оптимизации одного параметра движка test стратегии (multithread)
+#' 
+#' @param var.begin Стартовое значение оптимизации
+#' @param var.end Конечное значение оптимизации
+#'
+#' @return result DF с perfomance'ами по всем итерациям цикла 
+#'
+#' @export
+TestStr_ParallelOptimiser <- function(var.begin, var.end, ...) {
+  #
+  require(parallel)
+  # запуск кластера
+  parallel_cluster <- 
+    parallel::detectCores() %>%
+    parallel::makeCluster(.)
+  ## Подгрузка данных в кластер
+  # подгрузка библиотек
+  clusterEvalQ(parallel_cluster, {
+    library(quantmod)
+    library(magrittr)
+    library(tidyr)
+    library(PerformanceAnalytics)
+    source("str/libStrategy.R")
+    source("str/test/test_str_gen.R")
+    source("str/test/test_str_eva.R")
+    source("str/test/test_str_opt.R")
+    source("str/test/test_str_gear.R")
+  })
+  # подгрузка переменных
+  clusterExport(parallel_cluster, envir = .GlobalEnv, 
+    varlist = c(
+      "data.source.list", 
+      "add.per", "k.mm", "balance.start", 
+      "basket.weights", "sleeps", "commissions", "ret.type"
+    )
+  )
+  #
+  #system.time({
+  result <- 
+    var.begin:var.end %>%
+    parLapply(
+      parallel_cluster,
+      ., 
+      function(x){
+        TestStr_OneThreadRun(data.source = data.source.list[[1]],
+                             sma.per = x, add.per, k.mm, balance.start, 
+                             basket.weights, sleeps, commissions, ret.type)
+      }
+    ) %>% 
+    {
+      .[!is.na(.)]
+    } %>%
+    MergeData_inList_byRow(.)
+  #})
+  #
+  if(!is.null(parallel_cluster)) {
+    parallel::stopCluster(parallel_cluster)
+    parallel_cluster <- c()
+  }
+  #  
+  return(result)
+}
+#
+###
 #' Тупая функция оптимизации одного параметра движка test стратегии (на одном ядре)
 #' 
 #' @param var.begin Стартовое значение оптимизации
@@ -20,24 +84,24 @@
 TestStr_StupidOptimiser <- function(var.begin, var.end,
                                     data.source, add.per, k.mm, balance.start, 
                                     basket.weights, sleeps, commissions, ret.type) {
+  #
   system.time({
-   result <- 
-     var.begin:var.end %>%
-     lapply(., function(x){
-                 TestStr_OneTradeProbe(data.source = data.source.list[[1]],
+    result <- 
+      var.begin:var.end %>%
+      lapply(., function(x){
+                  TestStr_OneThreadRun(data.source = data.source.list[[1]],
                                        sma.per = x, add.per, k.mm, balance.start, 
                                        basket.weights, sleeps, commissions, ret.type)
                }
-     ) %>%
-     {
-       .[!is.na(.)]
-     } %>%
-     MergeData_inList_byRow(.)
+      ) %>%
+      {
+        .[!is.na(.)]
+      } %>%
+      MergeData_inList_byRow(.)
   })  
+  #
   return(result)
 }
-
-
 #
 ###
 #' Функция одного прогона вычислений движка test стратегии
@@ -54,10 +118,10 @@ TestStr_StupidOptimiser <- function(var.begin, var.end,
 #' @return list(data, data.state) Лист с данными отработки и данные сделок
 #'
 #' @export
-TestStr_OneTradeProbe <- function(data.source = data.source.list[[1]], 
-                                  sma.per, add.per, k.mm, basket.weights, 
-                                  sleeps, commissions,
-                                  balance.start, ret.type) {
+TestStr_OneThreadRun <- function(data.source = data.source.list[[1]], 
+                                 sma.per, add.per, k.mm, basket.weights, 
+                                 sleeps, commissions,
+                                 balance.start, ret.type) {
   ### 
   ## Отработка тестового робота
   data.strategy.list <- TestStr_gear(data.source, sma.per, add.per, k.mm, 
